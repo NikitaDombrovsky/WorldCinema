a221988e340a961295b6cb3f3a7e8774878e9bf7
NikitaDombrovsky <nikita.nikita55@mail.ru>
2022-03-22 13:11:00 +0600

    22.03.22 13:10

---
+++ app/src/main/java/com/example/worldcinema/AuthorizationScreen.java
@@ -0,0 +1,13 @@
+package com.example.worldcinema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class AuthorizationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+    }
+}
---
+++ app/src/main/java/com/example/worldcinema/RegistrationScreen.java
@@ -0,0 +1,14 @@
+package com.example.worldcinema;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
